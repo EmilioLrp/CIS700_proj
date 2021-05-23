c6ef9bd9cc00538db765952de43af7cdd337ac51
EmilioLIU <14251493@life.hkbu.edu.hk>
2021-05-23 16:36:46 -0400

    fix: data generator

--- data/generator.py
+++ data/generator.py
@@ -70,6 +70,14 @@ def check(file):
     length = len(x)
     print(length)
 
+def run():
+    train_file = "data/train.txt"
+    test_file = "data/test.txt"
+    test_file_var = "data/test_var.txt"
+    qh_generator(conf.get_train_data_size(), train_file, conf.get_input_size())
+    qh_generator(conf.get_test_data_size(), test_file, conf.get_input_size())
+    qh_generator(conf.get_test_data_size(), test_file_var, conf.var_input_size)
+
 
 if __name__ == '__main__':
     train_file = "train.txt"
--- data_generator.py
+++ data_generator.py
@@ -1,11 +1,4 @@
-from data.generator import qh_generator
-from data.config import Config
-conf = Config()
+from data.generator import run
 
 if __name__ == '__main__':
-    train_file = "data/train.txt"
-    test_file = "data/test.txt"
-    test_file_var = "data/test_var.txt"
-    qh_generator(conf.get_train_data_size(), train_file, conf.get_input_size())
-    qh_generator(conf.get_test_data_size(), test_file, conf.get_input_size())
-    qh_generator(conf.get_test_data_size(), test_file_var, conf.var_input_size)
+    run()
