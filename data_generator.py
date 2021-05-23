f879cc10717dff6bb26bd0dd3034984d0bbcb363
EmilioLIU <14251493@life.hkbu.edu.hk>
2021-05-23 16:21:37 -0400

    fix: data generator

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,4 @@
-from data.generator import mss_generator
+from data.generator import qh_generator
 from data.config import Config
 conf = Config()
 
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     train_file = "data/train.txt"
     test_file = "data/test.txt"
     test_file_var = "data/test_var.txt"
-    mss_generator(conf.get_train_data_size(), train_file, conf.get_input_size())
-    mss_generator(conf.get_test_data_size(), test_file, conf.get_input_size())
-    mss_generator(conf.get_test_data_size(), test_file_var, conf.var_input_size)
+    qh_generator(conf.get_train_data_size(), train_file, conf.get_input_size())
+    qh_generator(conf.get_test_data_size(), test_file, conf.get_input_size())
+    qh_generator(conf.get_test_data_size(), test_file_var, conf.var_input_size)
